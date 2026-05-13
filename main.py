9f3683c37791ceb0bb99c6381e4cc7525b12312d
anna0708120-a11y <anna0708120@gmail.com>
2026-05-13 20:39:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -614,3 +614,15 @@ scheduler.start()
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
+
+
+@app.post("/event")
+async def event(req: Request):
+    data = await req.json()
+
+    print(data)
+
+    return {"ok": True}
+
+
+
